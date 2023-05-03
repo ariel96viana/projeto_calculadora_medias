ead2483cf0327c643d9c59c23727aac548d2d56e
Ariel Oliveira <ariel96viana@gmail.com>
2023-05-03 11:22:34 -0300

    fix: limitação de casas decimais

--- main.js
+++ main.js
@@ -50,7 +50,8 @@ function atualizarTable() {
 function notaFinal() {
   const mediaFinal = calculaMedia();
 
-  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-valor").innerHTML =
+    mediaFinal.toFixed(2);
   document.getElementById("media-final-resultado").innerHTML =
     mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
